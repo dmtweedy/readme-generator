5eed33212b413c513b7a0a62254c9ad63b574b6b
dmtweedy <damon.tweedy@yahoo.com>
2023-07-26 15:14:59 -0500

    added js files

---
+++ Develop/index.js
@@ -0,0 +1,134 @@
+// TODO: Include packages needed for this application
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+// TODO: Create an array of questions for user input
+
+const questions = [
+  "Enter your GitHub username:",
+  "Enter your email address:",
+  "Enter your project title:", 
+  "Enter a description of your project:", 
+  "Enter the installation instructions for your project:", 
+  "Enter the usage information for your project:", 
+  "Enter the contribution guidelines for your project:", 
+  "Enter the test instructions for your project:",
+  "Please select the license you would like to use:",
+  "Enter instructions on how to contact you for additional information:"
+];
+
+// TODO: Create a function to write README file
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('README file generated successfully!');
+    }
+  });
+}
+
+// TODO: Create a function to initialize app
+
+function init() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'username',
+      message: questions[0],
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: questions[1],
+    },
+    {
+      type: 'input',
+      name: 'title',
+      message: questions[2],
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: questions[3],
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: questions[4],
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: questions[5],
+    },
+    {
+      type: 'input',
+      name: 'contributing',
+      message: questions[6],
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: questions[7],
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: questions[8],
+      choices: ['MIT', 'Apache 2.0', 'GNU GPLv3', 'ISC', 'None'],
+    },
+    {
+      type: 'input',
+      name: 'contact',
+      message: questions[9],
+    },
+  ])
+  .then((answers) => {
+    const readmeTemplate = 
+`# ${answers.title}
+
+## Description
+${answers.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${answers.installation}
+
+## Usage
+${answers.usage}
+
+## License
+${answers.license}
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.tests}
+
+## Questions
+My GitHub username: ${answers.username}
+My email address: ${answers.email}
+${answers.contact}
+`;
+
+    writeToFile('readme.md', readmeTemplate);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+}
+
+// Function call to initialize app
+init();
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,76 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+
+function renderLicenseBadge(license) {
+  if (license) {
+    return `[![License](license_badge_url)](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+
+function renderLicenseLink(license) {
+  if (license) {
+    return `[License](https://opensource.org/licenses/${license})`;
+  }
+  return '';
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
+
+This project is covered under the ${license} License. See [License](https://opensource.org/licenses/${license}) for more information.
+`;
+  }
+  return '';
+}
+
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+
+  return `# ${data.title}
+
+${licenseBadge}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${licenseSection ? '- [License](#license)\n' : ''}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+${licenseSection}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+My GitHub username: ${data.username}
+My email address: ${data.email}
+${data.contact ? `${data.contact}\n` : ''}
+`;
+}
+
+module.exports = generateMarkdown;
